37770f836bfb753a6c677f202faa06f8261f3a5d
cquest <cquest@openstreetmap.fr>
2019-09-14 10:45:54 +0200

    appel direct par bash

--- ods_all_update.sh
+++ ods_all_update.sh
@@ -3,6 +3,6 @@ EXCLUSIONS='^(\.|public.opendatasoft)'
 JOBS=4
 LOG="../ods-$(date +%Y%m%d).log"
 
-date -I > $LOG
-find . -maxdepth 1 -type d | sed 's!^.*/!!' | grep -v '^.$' | egrep -v "$EXCLUSIONS" | sort | parallel --line-buffer -j $JOBS ./ods_update.sh {} >> $LOG
-date -I >> $LOG
+date -Iseconds > $LOG
+find . -maxdepth 1 -type d | sed 's!^.*/!!' | grep -v '^.$' | egrep -v "$EXCLUSIONS" | sort | parallel --line-buffer -j $JOBS bash ods_update.sh {} >> $LOG
+date -Iseconds >> $LOG
